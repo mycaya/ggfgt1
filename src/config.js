af5368ab3e75d9049b7b3524a0a8443623546f20
mmacke16 <mmacke16@ford.com>
2021-04-19 23:16:06 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ config();
 
 export default {
   database: {
-    URI: process.env.MONGODB_URI || "mongodb://localhost/imgshare",
+    URI: process.env.MONGODB_URI || "mongodb://gg4gt1-14627:Jmcmp4HYEFFkwoXCTs1cj52DjDVkml@db-gg4gt1-14627.nodechef.com:5385/gg4gt1",
   },
   port: process.env.PORT || 3000,
 };
